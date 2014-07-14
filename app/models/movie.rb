d32cd4bcef8e515170e8d5441aeefba2360f4abb
Jason Witter <jason-witter@excella.com>
2014-07-14 15:19:02 -0400

    changed sort order for movie lists

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,5 +1,5 @@
 class Movie < ActiveRecord::Base
 	belongs_to :user
-	default_scope -> { order('created_at DESC') }
+	default_scope -> { order('title ASC') }
 	validates :user_id, presence: true
 end
