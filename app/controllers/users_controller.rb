47044f0e675ab281cd290c60d363dbd26b207df6
Jason Witter <jason-witter@excella.com>
2014-07-14 13:40:04 -0400

    added edit user  function

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,7 @@
 class UsersController < ApplicationController
+  before_action :signed_in_user,  only: [:edit, :update]
+  before_action :correct_user,   only: [:edit, :update]
+
   def show
   	@user = User.find(params[:id])
   end
@@ -12,9 +15,20 @@ class UsersController < ApplicationController
     if @user.save
       sign_in @user
       flash[:success] = "Welcome to the Sample App!"
-      redirect_to @user
+      redirect_to movies_path
     else
       render 'new'
+    end
+  end
+
+  def edit
+  end
+
+  def update
+    if @user.update_attributes(user_params)
+      redirect_to movies_path
+    else
+      render 'edit'
     end
   end
 
@@ -23,5 +37,14 @@ class UsersController < ApplicationController
     def user_params
       params.require(:user).permit(:name, :password,
                                    :password_confirmation)
+    end
+
+    def signed_in_user
+      redirect_to signin_url, notice: "Please sign in." unless signed_in?
+    end
+
+    def correct_user
+      @user = User.find(params[:id])
+      redirect_to(root_url) unless current_user?(@user)
     end
 end
