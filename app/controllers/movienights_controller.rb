bd42ff642db0d1347c25a5a11a417d67fddf3567
Jason Witter <jason-witter@excella.com>
2014-07-15 10:11:04 -0400

    fixing movienight controller, I guess this is why people write tests

--- app/controllers/movienights_controller.rb
+++ app/controllers/movienights_controller.rb
@@ -7,7 +7,7 @@ class MovienightsController < ApplicationController
 	end
 
 	def show
-		count = get_movie_night_movies.count
-		@random_movie =  get_movie_night_movies[choose_movie(count)]
+		count = get_movienight_movies.count
+		@random_movie =  get_movienight_movies[choose_movie(count)]
 	end
 end
