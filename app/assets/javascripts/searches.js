0eab7898afc0522be078a822106a00f87bd25a6b
Jason Witter <jason-witter@excella.com>
2014-07-16 09:40:33 -0400

    Refined search page

--- app/assets/javascripts/searches.js
+++ app/assets/javascripts/searches.js
@@ -1,5 +1,5 @@
 $(document).ready(function() {
-  var call_search = search;
+  // Search triggers
   $('#search_text').keypress(function(e) {
     if (e.which == 13) {
         search();
@@ -7,6 +7,7 @@ $(document).ready(function() {
   });
   $(document.getElementById('search_button')).click(search);
 
+  // Rotten Tomatoes search function
   function search() {
     var apikey = "gmpx67fws5dfu7h9g9qj4azx";
     var baseUrl = "http://api.rottentomatoes.com/api/public/v1.0";
@@ -23,17 +24,42 @@ $(document).ready(function() {
       success: searchCallback
     });
 
-      // callback for when we get back the results
+    // callback for when we get back the results
     function searchCallback(data) {
-      var document_body = $(document.getElementById('search_body'));
-      document_body.html('');
-      document_body.append('Found ' + data.total + ' results for ' + query + '. Displaying the top 10.');
+      var search_result_body = $(document.getElementById('search_body'));
+      search_result_body.html('');
+      var content = '';
+      if (data.total >=  10) {
+        content += '<p>Found ' + data.total + ' results for ' + query + '. Displaying the top 10.</p>';
+      } else if (data.total == 1) {
+        content += '<p>Found ' + data.total + ' result for ' + query + '.';
+      } else {
+        content += '<p>Found ' + data.total + ' results for ' + query + '.';
+      }
+      content += generate_table_head();
       var movies = data.movies;
       $.each(movies, function(index, movie) {
-       document_body.append('<h4>' + movie.title + '</h4>');
-       document_body.append('<img src="' + movie.posters.thumbnail + '" />');
-       document_body.append('<h4>' + movie.release_dates.dvd + '</h4>');
-     });
+        content += '<tr><td><img src="' + movie.posters.thumbnail + '" /></td>';
+        content += '<td>' + movie.title + '</td>';
+        content += '<td>' + movie.release_dates.dvd + '</td></tr>';
+      });
+      content += generate_table_foot();
+      search_result_body.html(content);
+    };
+
+    function generate_table_head() {
+      var str = '';
+      str += '<div class="row"><div class="col-md-8">' + 
+              '<table class="table table-striped">' + 
+              '<thead><tr><th></th><th>Title</th><th>DVD Release Date</th>' + 
+              '</tr></thead><tbody>';
+      return str;
+    };
+
+    function generate_table_foot() {
+      var str = '';
+      str += '</tbody></table></div></div>';
+      return str;
     };
   }
 });
